9a2ecef59d954879c042057b5baa7131e4c51b38
vliesaputra <veronica.liesaputra@gmail.com>
2013-12-29 18:04:46 +1300

    Remove duplicate import

--- src/com/vliesaputra/cordova/plugins/DeviceInformation.java
+++ src/com/vliesaputra/cordova/plugins/DeviceInformation.java
@@ -4,7 +4,6 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
